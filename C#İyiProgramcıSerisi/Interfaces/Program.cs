a9da983f71b3b586ed3255f9d807364e1bbc263a
isademr <56588468+isademr@users.noreply.github.com>
2023-04-05 11:20:01 +0300

    Interfaces
    Interfaces

---
+++ C#İyiProgramcıSerisi/Interfaces/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Interfaces
+{
+     class Program
+     {
+        static void Main(string[] args)
+        {
+            //IPersonManager customerManager = new CustomerManager();
+
+            //IPersonManager employeeManager = new EmployeeManager();
+
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new InternManager());
+        }
+     }
+
+    interface IPersonManager
+    {
+        //unimplemented operation
+         void Add();
+        void Update();
+        
+    }
+
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            //müşteri ekleme kodları
+            Console.WriteLine("Müşteri Eklendi.");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri Güncellendi");
+        }
+    }
+
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            //çalışan ekleme kodları
+            Console.WriteLine("Çalışan eklendi.");
+             
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Çalışan güncellendi");
+        }
+    }
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajyer eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajyer güncellendi");
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            
+            personManager.Add();
+        
+        }
+
+
+      
+
+    }
+}
